4c5247a32a1dccab455e5b137578fe7b8feba7ae
EC2 Default User <ryutake0306@gmail.com>
2021-03-11 14:02:44 +0000

    [update]3-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,15 @@
 class BooksController < ApplicationController
   def index
+    @books=Book.all
+    @book=Book.new(book_params)
   end
 
   def show
+    @
   end
 
   def new
+    @book=Book.new
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
  resources :books
 
- root to: 'homes#top'
+ root 'homes#top'
 
 end
