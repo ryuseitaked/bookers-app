78af9b9a1d6db4de8e7b04dba917548dacc49568
EC2 Default User <ryutake0306@gmail.com>
2021-03-11 15:38:28 +0000

    [update]3-8

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
- resources :books
 
  root 'homes#top'
+ root 'books#top'
+ get 'books' =>'books#index'
+ post 'books' =>'books#create'
+ get 'books/:id' =>'books#show', as: 'book'
+ get 'books/:id/edit' => 'boos#edit', as: 'edit_book'
+ patch 'books/:id' => 'books#update', as: 'update_book'
+ delete 'books/:id' =>'book#destroy', as: 'destroy_book'
 
 end
